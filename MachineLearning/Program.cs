ff1622e0e747d13d71692be558497e376cfb289e
Philipp C. Moser <dev@philipp-moser.de>
2019-03-16 11:53:22 +0100

    NestedClass "IrisData": Data-Structure-Definition

--- MachineLearning/Program.cs
+++ MachineLearning/Program.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ML.Data;
 
 namespace MachineLearning
 {
     class Program
     {
+
+        // Data-Structure-Definition
+        public class IrisData
+        {
+            [LoadColumn(0)] public float SepalLength;
+            [LoadColumn(1)] public float SepalWidth;
+            [LoadColumn(2)] public float PetalLength;
+            [LoadColumn(3)] public float PetalWidth;
+            [LoadColumn(4)] public float Label;
+        }
+
+
+
         static void Main(string[] args)
         {
         }
